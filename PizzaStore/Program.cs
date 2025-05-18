897625690b9da8e793379c7a803999f2fe10759d
Tong <163929652+Tong-Ye-1159668@users.noreply.github.com>
2025-05-18 14:32:16 +1200

    initial db and api routes

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.OpenApi.Models;
+using PizzaStore.DB;
 
 var builder = WebApplication.CreateBuilder(args);
     
@@ -18,7 +19,13 @@ if (app.Environment.IsDevelopment())
       c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
    });
 }
-    
+
 app.MapGet("/", () => "Hello World!");
-    
+
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+
 app.Run();
